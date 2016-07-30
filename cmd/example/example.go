b8d9541f304c50209a47b31a0d5cc08d42f0b858
Dan Pupius <dan@medium.com>
2016-07-30 16:04:14 -0700

    Adds an example server that proxies to bbc and wikipedia

--- cmd/example/example.go
+++ cmd/example/example.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"github.com/dpup/locus"
+	"time"
 )
 
 func main() {
 
 	proxy := locus.New()
+	proxy.VerboseLogging = true
 
+	// open http://localhost:5555/news/world-middle-east-36932694
+	bbc := proxy.NewConfig()
+	bbc.Match("/news")
+	bbc.Upstream("http://www.bbc.com/news")
+	bbc.StripHeader("Cookie")
+	bbc.SetHeader("Host", "www.bbc.com")
+	bbc.SetHeader("Referer", "http://www.bbc.com/news")
+
+	// open localhost:5555/wiki/England
 	wiki := proxy.NewConfig()
 	wiki.Match("/wiki")
-	wiki.Upstream("https://en.wikipedia.org/wiki/")
+	wiki.Upstream("https://en.wikipedia.org")
 	wiki.StripHeader("Cookie")
+	wiki.SetHeader("Host", "en.wikipedia.org")
 	wiki.SetHeader("Referer", "https://en.wikipedia.org/wiki/Main_Page")
+
+	panic(proxy.Serve(5555, 10*time.Second, 10*time.Second))
 }
