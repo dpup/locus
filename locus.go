9a9e18ee8e1d058ac1c7fa065b159e210c2b5d53
Dan Pupius <dan@medium.com>
2016-08-02 09:23:46 -0700

    RevProxy > Locus to disambiguate with ReverseProxy

--- locus.go
+++ locus.go
@@ -9,8 +9,8 @@ import (
 	"time"
 )
 
-// RevProxy wraps golang's httputil.ReverseProxy to provide multi-host routing.
-type RevProxy struct {
+// Locus wraps golang's httputil.ReverseProxy to provide multi-host routing.
+type Locus struct {
 
 	// VerboseLogging specifies that additional request details should be logged.
 	VerboseLogging bool
@@ -27,95 +27,95 @@ type RevProxy struct {
 	proxy   *ReverseProxy
 }
 
-// New returns an empty instance of a RevProxy.
-func New() *RevProxy {
-	rp := &RevProxy{}
-	rp.configs = []*Config{}
-	rp.proxy = &ReverseProxy{Director: rp.director}
-	return rp
+// New returns an empty instance of a Locus.
+func New() *Locus {
+	locus := &Locus{}
+	locus.configs = []*Config{}
+	locus.proxy = &ReverseProxy{Director: locus.director}
+	return locus
 }
 
 // NewConfig creates an empty config, registers it, then returns it.
-func (rp *RevProxy) NewConfig() *Config {
-	cfg := &Config{Name: fmt.Sprintf("cfg%d", len(rp.configs))}
-	rp.AddConfig(cfg)
+func (locus *Locus) NewConfig() *Config {
+	cfg := &Config{Name: fmt.Sprintf("cfg%d", len(locus.configs))}
+	locus.AddConfig(cfg)
 	return cfg
 }
 
 // AddConfig adds config to the reverse proxy. Configs will be checked in the
 // order they were added, the first matching config being used to route the
 // request.
-func (rp *RevProxy) AddConfig(cfg *Config) {
-	rp.configs = append(rp.configs, cfg)
+func (locus *Locus) AddConfig(cfg *Config) {
+	locus.configs = append(locus.configs, cfg)
 }
 
 // LoadConfigs adds site configs stored as YAML. See SampleYAMLConfig.
-func (rp *RevProxy) LoadConfigs(data []byte) error {
+func (locus *Locus) LoadConfigs(data []byte) error {
 	cfgs, err := loadConfigsFromYAML(data)
 	if err != nil {
 		return err
 	}
 	for _, cfg := range cfgs {
-		rp.AddConfig(cfg)
+		locus.AddConfig(cfg)
 	}
 	return nil
 }
 
 // LoadConfigFile reads configs from a YAML file.
-func (rp *RevProxy) LoadConfigFile(filename string) error {
+func (locus *Locus) LoadConfigFile(filename string) error {
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return err
 	}
-	return rp.LoadConfigs(data)
+	return locus.LoadConfigs(data)
 }
 
 // Serve starts a server.
-func (rp *RevProxy) Serve(port uint16, readTimeout, writeTimeout time.Duration) error {
+func (locus *Locus) Serve(port uint16, readTimeout, writeTimeout time.Duration) error {
 	s := http.Server{
 		Addr:           fmt.Sprintf(":%d", port),
-		Handler:        rp,
+		Handler:        locus,
 		ReadTimeout:    readTimeout,
 		WriteTimeout:   writeTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
-	rp.elogf("Starting RevProxy on port %d", port)
+	locus.elogf("Starting Locus on port %d", port)
 	return s.ListenAndServe()
 }
 
-func (rp *RevProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	rp.proxy.ServeHTTP(rw, req)
+func (locus *Locus) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	locus.proxy.ServeHTTP(rw, req)
 }
 
-func (rp *RevProxy) director(req *http.Request) {
-	for _, c := range rp.configs {
+func (locus *Locus) director(req *http.Request) {
+	for _, c := range locus.configs {
 		if c.Matches(req) {
 			err := c.Transform(req)
 			if err != nil { // TODO: Render local error page.
-				rp.elogf("Error transforming request: %s", err)
+				locus.elogf("Error transforming request: %s", err)
 			}
-			if rp.VerboseLogging {
+			if locus.VerboseLogging {
 				d, _ := httputil.DumpRequestOut(req, false)
-				rp.alogf("locus[%s] %s %s://%s %s", c.Name, req.RemoteAddr, req.URL.Scheme, req.URL.Host, string(d))
+				locus.alogf("locus[%s] %s %s://%s %s", c.Name, req.RemoteAddr, req.URL.Scheme, req.URL.Host, string(d))
 			} else {
-				rp.alogf("locus[%s] %s %s %s://%s%s", c.Name, req.RemoteAddr, req.Method, req.URL.Scheme, req.URL.Host, req.URL.Path)
+				locus.alogf("locus[%s] %s %s %s://%s%s", c.Name, req.RemoteAddr, req.Method, req.URL.Scheme, req.URL.Host, req.URL.Path)
 			}
 			return
 		}
 	}
 }
 
-func (rp *RevProxy) alogf(format string, args ...interface{}) {
-	if rp.AccessLog != nil {
-		rp.AccessLog.Printf(format, args...)
+func (locus *Locus) alogf(format string, args ...interface{}) {
+	if locus.AccessLog != nil {
+		locus.AccessLog.Printf(format, args...)
 	} else {
 		log.Printf(format, args...)
 	}
 }
 
-func (rp *RevProxy) elogf(format string, args ...interface{}) {
-	if rp.ErrorLog != nil {
-		rp.ErrorLog.Printf(format, args...)
+func (locus *Locus) elogf(format string, args ...interface{}) {
+	if locus.ErrorLog != nil {
+		locus.ErrorLog.Printf(format, args...)
 	} else {
 		log.Printf(format, args...)
 	}
